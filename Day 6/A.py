d50b5dbb74a753530147698017e51331bccb42c0
Patchkat <24392778+Patchkat@users.noreply.github.com>
2021-12-05 23:26:42 -0700

    Day 6

---
+++ Day 6/A.py
@@ -0,0 +1,12 @@
+for x in open("./input.txt").readlines():
+    fish = [int(y) for y in x.split(",")]
+time = 0
+while time < 80:
+    for i, x in enumerate(fish):
+        if x != 0:
+            fish[i] = fish[i] - 1
+        else:
+            fish[i] = 6
+            fish.append(9)
+    time += 1
+print(len(fish))
---
+++ Day 6/B.py
@@ -0,0 +1,14 @@
+for x in open("./input.txt").readlines():
+    fish = [int(y) for y in x.split(",")]
+fish_count = [0 for x in range(9)]
+day = 0
+for x in fish:
+    fish_count[x] += 1
+while day < 256:
+    new_fish = fish_count[0]
+    for x in range(1, 9):
+        fish_count[x-1] = fish_count[x]
+    fish_count[8] = new_fish
+    fish_count[6] += new_fish
+    day += 1
+print(sum(fish_count))
